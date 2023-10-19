4d1144f5824824830f91f2bcefa985e89816ced5
xarp2000 <138525203+xarp2000@users.noreply.github.com>
2023-10-18 21:09:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menuListArray = ["Pizza Vegetariana",//adicionar mais itens
+                 "Pizza De Calabresa" 
+                 "Pizza De Frango"
+                 "Pizza De Queijo"   
+                 "Pizza De Mortadela"   ];
+
+function getMenu(){
+var htmldata;
+menuListArray.sort();
+for(var i=0; i<menuListArray.length;i++){
+htmldata=htmldata+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata
+}
+
+function addItem(){
+var htmldata;
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
+var item=document.getElementById("addItem").value;
+menuListArray.sort();
+htmldata=""
+for(var i=0; i<menuListArray.length;i++){
+    htmldata=htmldata+imgtags+ menuListArray[i] + '<br>'
+}
+document.getElementById("displayMenu").innerHTML = htmldata
+}
+
+function addTop(){
+var item=document.getElementById("addItem").value;
+menuListArray.push(item);
+ addItem()
+}
